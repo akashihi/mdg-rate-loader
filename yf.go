46b3c2ca1bf44f6a101455dfb1525f497b160a7b
Denis Chaplygin <dchaplyg@redhat.com>
2017-09-21 14:10:18 +0200

    Added YahooFinance data downloader.

---
+++ yf.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/NodePrime/jsonpath"
+	"github.com/shopspring/decimal"
+)
+
+func getRate(rate *RateRecord) (*RateRecord){
+	var netClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	pair := fmt.Sprintf("%s%s=X", rate.FromCode, rate.ToCode)
+	log.Debug("Loading rate for %s pair", pair)
+
+	u, err := url.Parse("https://query1.finance.yahoo.com/v7/finance/spark?range=1d&interval=60m&indicators=close&includeTimestamps=true&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance")
+	if err != nil {
+		log.Warning("Unable to construct UahooFinance url: %v", err)
+		return nil
+	}
+	q := u.Query()
+	q.Set("symbols", pair)
+	u.RawQuery = q.Encode()
+
+
+	response, err := netClient.Get(u.String())
+	if err != nil {
+		log.Warning("Unable to retrieve ")
+	}
+
+	ratePath := "$.spark.result[0].response[0].indicators.quote[0].close[*]+"
+	paths, err := jsonpath.ParsePaths(ratePath)
+	if err != nil {
+		log.Critical("Invalid JsonPath: %v", err)
+		return nil
+	}
+	eval, err := jsonpath.EvalPathsInReader(response.Body, paths)
+	if err != nil {
+		log.Warning("Unable to parse YahooFinance json: %v", err)
+		return nil
+	}
+	var rates []string
+	for {
+		if result, ok := eval.Next(); ok {
+			val := string(result.Value[:])
+			if val != "null" {
+				rates = append(rates, val)
+			}
+		} else {
+			break
+		}
+	}
+
+	rate.Rate, err = decimal.NewFromString(rates[len(rates)-1])
+	if err != nil {
+		log.Warning("Unable to parse rate value: %v", err)
+		return nil
+	}
+
+	log.Info("Rate for %s%s pair is %s", rate.FromCode, rate.ToCode, rate.Rate.String())
+	return rate
+}
+
