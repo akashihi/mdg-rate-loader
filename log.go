a06841be14874cb635cc2391d90246cba35a28f2
Denis Chaplygin <dchaplyg@redhat.com>
2017-09-21 14:19:09 +0200

    Added logging.

---
+++ log.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/op/go-logging"
+	"os"
+)
+
+var log = logging.MustGetLogger("mdg-rate-loader")
+var format = logging.MustStringFormatter(
+	"%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}",
+)
+
+func InitLog() {
+	backend := logging.NewLogBackend(os.Stdout, "", 0)
+	backendFormatter := logging.NewBackendFormatter(backend, format)
+	logging.SetBackend(backendFormatter)
+}
